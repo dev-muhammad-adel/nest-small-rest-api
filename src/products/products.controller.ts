5bd3b778542cc8deb321e82dd911d02e03d92a8f
mohamed adel <mohamed.adel@appssquare.com>
2021-11-27 12:25:52 +0200

    static route Get decorator/ use route parameter

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
    constructor(private readonly proudctsService: ProductsService) {
    }
-   @Get()
+   // use static route
+   @Get('phones')
    getProducts(): string {
       return this.proudctsService.getProducts()
    }
+
+   // use param route (dynamic param)
+   @Get(':id/:name')
+   getProduct(@Param() params, @Param('name') name): string {
+      return `ur id : #${params.id} and ur name : ${name}`
+   }
+
+
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class ProductsService {
    getProducts(): string {
-      return 'products'
+      return 'this action get all products'
    }
 }
